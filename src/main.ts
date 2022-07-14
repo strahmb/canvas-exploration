6a68eba2cb3e2b5d1e41a1fe670bc2f7823765cf
Brendan Strahm <bstrahm@gmail.com>
2022-07-14 18:41:17 +0000

    Refactoring stuff in the main file

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,76 @@
-var canvas = <HTMLCanvasElement> document.getElementById("myCanvas");
-var ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
+var canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+var ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 
-let tileSize = 40;
-let heroPosition = {
-    x: 20,
-    y: 40
-};
-let heroSize = 40;
+const FPS = 60;
+
+const TILE_SIZE = 40;
+
+class Hero {
+    position: { x: number; y: number };
+    size: number;
+
+    constructor(position = { x: 20, y: 40 }) {
+        this.size = 40;
+        this.position = position;
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.rect(hero.position.x, hero.position.y, hero.size, hero.size);
+        ctx.fillStyle = "#FF0000";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
 let keyPress = {
     right: false,
-    left: false
-}
+    left: false,
+};
+let _stop = false;
 
-
+let hero = new Hero();
 function render() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-
-    if(keyPress.right) {
-        heroPosition.x += 7;
-        if (heroPosition.x + heroSize > canvas.width){
-            heroPosition.x = canvas.width - heroSize;
+    // Handle events
+    if (!_stop) {
+        if (keyPress.right) {
+            hero.position.x += 7;
+            if (hero.position.x + hero.size > canvas.width) {
+                hero.position.x = canvas.width - hero.size;
+            }
+        } else if (keyPress.left) {
+            hero.position.x -= 7;
+            if (hero.position.x < 0) {
+                hero.position.x = 0;
+            }
         }
     }
-    else if(keyPress.left) {
-        heroPosition.x -= 7;
-        if (heroPosition.x < 0){
-            heroPosition.x = 0;
-        }
-    }
-    
 
-    ctx.beginPath();
-    ctx.rect(heroPosition.x, heroPosition.y, heroSize, heroSize);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath();
+    // Draw
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    hero.draw(ctx);
 }
-setInterval(render, 10);
+setInterval(render, 1000 / FPS);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if (e.key == "Right" || e.key == "ArrowRight") {
         keyPress.right = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
         keyPress.left = true;
     }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if (e.key == "Right" || e.key == "ArrowRight") {
         keyPress.right = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
         keyPress.left = false;
     }
 }
-
 
 // @ts-ignore
 if (module.hot) {
