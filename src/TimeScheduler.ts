02c891472c33af658bdbb6ba90aa7bb55a32d621
Brendan Strahm <bstrahm@gmail.com>
2022-07-14 18:40:34 +0000

    Implemented scheduler

--- src/TimeScheduler.ts
+++ src/TimeScheduler.ts
@@ -1,34 +1,6 @@
 import { PriorityQueue } from "./PriorityQueue";
 import { Event } from "./Event";
 
-// def __init__(self):
-//         self.__scheduledEvents = PriorityQueue()
-
-//     def scheduleEvent(self, event, delay = 0.0):
-//         """
-//             Schedules an event to occur after a certain delay.
-//         """
-
-//         if event is not None:
-//             self.__scheduledEvents.enqueue(event, delay)
-
-//     def nextEvent(self):
-//         """
-//             Dequeues and returns the next event to occur.
-//         """
-
-//         time, event = self.__scheduledEvents.dequeueWithKey()
-//         self.__scheduledEvents.adjustPriorities(-time)
-
-//         return event
-
-//     def cancelEvent(self, event):
-//         """
-//             Cancels a pending event.
-//         """
-
-//         self.__scheduledEvents.erase_ref(event)
-
 export class TimeScheduler {
     private _scheduledEvents: PriorityQueue = new PriorityQueue();
 
@@ -44,5 +16,9 @@ export class TimeScheduler {
             this._scheduledEvents.adjustPriorities(-event.priority);
         }
         return event?.value;
+    }
+
+    cancelEvent(event: Event) {
+        this._scheduledEvents.erase(event);
     }
 }
